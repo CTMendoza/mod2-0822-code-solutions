24f6d05e11961148a0f0a336f0ee3edab3860af3
Joe King <tiptop1112@gmail.com>
2022-08-25 03:26:20 +0000

    Completed javascript-bank

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -14,3 +14,29 @@ Account.prototype.deposit = function (amount) {
     return false;
   }
 };
+
+Account.prototype.withdraw = function (amount) {
+  var transaction = new Transaction('withdrawal', amount);
+  if (amount > 0 && Number.isInteger(amount)) {
+    this.transactions.push(transaction);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  if (this.transactions.length === 0) {
+    return 0;
+  } else {
+    var balance = 0;
+    for (var i = 0; i < this.transactions.length; i++) {
+      if (this.transactions[i].type === 'deposit') {
+        balance += this.transactions[i].amount;
+      } else if (this.transactions[i].type === 'withdrawal') {
+        balance -= this.transactions[i].amount;
+      }
+    }
+    return balance;
+  }
+};
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,38 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  var account = new Account(this.nextAccountNumber, holder);
+  if (balance > 0 && Number.isInteger(balance)) {
+    account.deposit(balance);
+    this.nextAccountNumber++;
+    this.accounts.push(account);
+    return account.number;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  if (this.accounts.length === 0) {
+    return 0;
+  } else {
+    var total = 0;
+    for (var i = 0; i < this.accounts.length; i++) {
+      total += this.accounts[i].getBalance();
+    }
+    return total;
+  }
+};
