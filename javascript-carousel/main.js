3e0f260b8ee7be32c84fa31111bbca7d126b3daa
Joe King <tiptop1112@gmail.com>
2022-08-28 19:12:58 +0000

    Completed javascript-carousel

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -12,11 +12,11 @@ $previousIcon.addEventListener('click', $handlePreviousIcon);
 function $handlePreviousIcon(event) {
   $iContainerChildren[currentCarouselItem].setAttribute('class', 'fa-regular fa-circle margin-right');
   if (currentCarouselItem === 0) {
-    currentCarouselItem = pokemonImages.length - 1;
+    currentCarouselItem = poke.length - 1;
   } else {
     currentCarouselItem -= 1;
   }
-  $img.setAttribute('src', pokemonImages[currentCarouselItem]);
+  $img.setAttribute('src', poke[currentCarouselItem].img);
   $iContainerChildren[currentCarouselItem].setAttribute('class', 'fa-solid fa-circle margin-right');
 }
 // create and click event listener function to check the next icon click event fires
@@ -24,37 +24,73 @@ $nextIcon.addEventListener('click', $handleNextIcon);
 
 function $handleNextIcon(event) {
   // console.log('clicking $nextIcon works');
-  // for (var i = 0; i < pokemonImages.length; i++) {
-  //   if ($img.getAttribute('src') === pokemonImages[i]) {
-  //     $img.setAttribute('src', pokemonImages[i += 1]);
-  //   } else if ($img.getAttribute('src') === pokemonImages[pokemonImages.length - 1]) {
-  //     $img.setAttribute('src', pokemonImages[0]);
+  // for (var i = 0; i < poke.length; i++) {
+  //   if ($img.getAttribute('src') === poke[i]) {
+  //     $img.setAttribute('src', poke[i += 1]);
+  //   } else if ($img.getAttribute('src') === poke[poke.length - 1]) {
+  //     $img.setAttribute('src', poke[0]);
   //   }
   // }
   $iContainerChildren[currentCarouselItem].setAttribute('class', 'fa-regular fa-circle margin-right');
   currentCarouselItem += 1;
-  if (currentCarouselItem === pokemonImages.length) {
+  if (currentCarouselItem === poke.length) {
     currentCarouselItem = 0;
   }
-  $img.setAttribute('src', pokemonImages[currentCarouselItem]);
+  $img.setAttribute('src', poke[currentCarouselItem].img);
   $iContainerChildren[currentCarouselItem].setAttribute('class', 'fa-solid fa-circle margin-right');
 }
 
-// create an array consisting of the image file paths
+// create an array consisting objects that contain properties for the image file paths amd methods
 
-var pokemonImages = [
-  'images/001.png',
-  'images/004.png',
-  'images/007.png',
-  'images/025.png',
-  'images/039.png'
+var poke = [
+  {
+    img: 'images/001.png',
+    imgClick: function (event) {
+      $handleSpecificIcon(0);
+    }
+  },
+  {
+    img: 'images/004.png',
+    imgClick: function (event) {
+      $handleSpecificIcon(1);
+    }
+  },
+  {
+    img: 'images/007.png',
+    imgClick: function (event) {
+      $handleSpecificIcon(2);
+    }
+  },
+  {
+    img: 'images/025.png',
+    imgClick: function (event) {
+      $handleSpecificIcon(3);
+    }
+  },
+  {
+    img: 'images/039.png',
+    imgClick: function (event) {
+      $handleSpecificIcon(4);
+    }
+  }
 ];
+
+function $handleSpecificIcon(specificItem) {
+  $iContainerChildren[currentCarouselItem].setAttribute('class', 'fa-regular fa-circle margin-right');
+  currentCarouselItem = specificItem;
+  $img.setAttribute('src', poke[currentCarouselItem].img);
+  $iContainerChildren[currentCarouselItem].setAttribute('class', 'fa-solid fa-circle margin-right');
+}
 
 var $iContainer = document.getElementById('progress-container');
 var $iContainerChildren = $iContainer.children;
 
-$iContainer.addEventListener('click', $nextProgressIcon);
-
-function $nextProgressIcon(event) {
-  $handleNextIcon();
+for (var i = 0; i < $iContainerChildren.length; i++) {
+  $iContainerChildren[i].addEventListener('click', poke[i].imgClick);
 }
+
+var autoNext = function () {
+  setInterval($handleNextIcon, 3000);
+};
+
+window.addEventListener('load', autoNext);
