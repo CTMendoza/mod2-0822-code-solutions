c51ae4f2cdf9ac77ce346c320e682096ad473752
Joe King <tiptop1112@gmail.com>
2022-10-05 16:36:29 +0000

    Fixed node-fs-readfile exercise.

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,24 +1,6 @@
 const fs = require('fs');
-var myArgs = process.argv.slice(2);
-
-if (myArgs[0] === 'cunningham.txt') {
-  fs.readFile('cunningham.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-} else if (myArgs[0] === 'dijkstra.txt') {
-  fs.readFile('dijkstra.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-} else if (myArgs[0] === 'hipster-ipsum.txt') {
-  fs.readFile('hipster-ipsum.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-} else if (myArgs[0] === 'hopper.txt') {
-  fs.readFile('hopper.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-}
+var myArgs = process.argv[2];
+fs.readFile(myArgs, 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
